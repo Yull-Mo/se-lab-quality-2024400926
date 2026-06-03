7e6c62004f60a89c78bf89ac3570ce777577e7df
Yull-Mo <1376466979@qq.com>
2026-06-03 17:06:09 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py（简易Flask Web）
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "DevOps Test Page Success"
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
